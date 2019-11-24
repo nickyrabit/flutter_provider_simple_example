179e8bae7ec4761a9e4e81482b6c3abb69d155ba
Nicky Ngailo <nickyrabit@gmail.com>
2019-11-24 18:04:58 +0300

    this is Loading data from servrer using providers to provide

--- lib/main.dart
+++ lib/main.dart
@@ -1,67 +1,133 @@
-import 'package:bloc_tutorial/Counter.dart';
 import 'package:flutter/material.dart';
+
 import 'package:provider/provider.dart';
+
+import 'app_state.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: ChangeNotifierProvider<Counter>(
-        builder: (_)=> Counter(0),
-        child: HomePage(),
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ChangeNotifierProvider<AppState>(
+          builder: (_) => AppState(),
+          child: MyHomePage(),
+        ));
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+
+    final appState = Provider.of<AppState>(context);
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              TextDisplay(),
+              TextEditWidget(),
+              RaisedButton(
+                onPressed: ()=> appState.fetchData(),
+                child: Text("Fetching data"),
+              ),
+
+              ResponseDisplay(),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class ResponseDisplay extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    final counter = Provider.of<Counter>(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Provider Demo"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+    final appState = Provider.of<AppState>(context);
+    return SingleChildScrollView(
+      padding: const EdgeInsets.all(16.0),
+      child: appState.isFetching ? CircularProgressIndicator() : appState.getResponseJson() != null ? ListView.builder(
+
+        primary: false,
+        shrinkWrap: true,
+        itemCount: appState.getResponseJson().length,
+        itemBuilder: (context, index) {
+
+          return ListTile(
+
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  appState.getResponseJson()[index]['avatar']),
             ),
-            Text(
-              '${counter.getCounter()}',
-              style: Theme.of(context).textTheme.display1,
+            title: Text(
+              appState.getResponseJson()[index]["first_name"],
             ),
-          ],
+
+          );
+        },
+      )
+          : Text("Press Button above to fetch data"),
+    );
+  }
+}
+
+class TextEditWidget extends StatefulWidget {
+  @override
+  _TextEditWidgetState createState() => _TextEditWidgetState();
+}
+
+
+class _TextEditWidgetState extends State<TextEditWidget> {
+  TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final appState = Provider.of<AppState>(context);
+
+    return Container(
+      child: TextField(
+        controller: _textEditingController,
+        decoration: InputDecoration(
+          labelText: "Some Text",
+          border: OutlineInputBorder(),
         ),
+        onChanged: (changed) => appState.setDisplayText(changed),
+        onSubmitted: (submitted) => appState.setDisplayText(submitted),
       ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          FloatingActionButton(
-            onPressed: () {
-              counter.increment();
-            },
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
-          SizedBox(height: 10),
-          FloatingActionButton(
-            onPressed: () {
-              counter.decrement();
-            },
-            tooltip: 'Increment',
-            child: Icon(Icons.remove),
-          )
-        ],
-      ),
+    );
+  }
+}
+
+
+class TextDisplay extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appState  = Provider.of<AppState>(context);
+    // TODO: implement build
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Text(appState.getDisplayText,
+      style: TextStyle(fontSize: 24),),
     );
   }
 }
